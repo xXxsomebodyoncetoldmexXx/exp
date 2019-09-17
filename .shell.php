770e3c2f8e32efe3e07eda4759898d3cc797a039
xXxsomebodyoncetoldmexXx <csgo0507@gmail.com>
2019-09-17 15:23:54 +0700

    DCTF2019

--- .shell.php
+++ .shell.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
  <html lang="en">
    <head>
-     <meta char-set="utf-8">
+    <meta char-set="utf-8">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+		<meta content="utf-8" http-equiv="encoding">
    </head>
    <body>
    </body>
