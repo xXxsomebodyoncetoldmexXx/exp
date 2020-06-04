3724f6c43bb2b4d8d18bc0cb16687da3f5414dea
Fox <31753655+xXxsomebodyoncetoldmexXx@users.noreply.github.com>
2020-06-04 22:51:52 +0700

    Create car.bat

---
+++ windows/car.bat
@@ -0,0 +1,9 @@
+@echo off
+Rem This program compile and run cpp file
+if not [%1]==[] (
+  START /B /WAIT g++ -std=c++17 -Wall -Wextra -Ofast -DLOCAL_PROJECT %1
+  if %ERRORLEVEL% == 0 (
+		echo [RUN]
+		a.exe
+	)
+) else (echo "[USAGE]: car <file.cpp>")
