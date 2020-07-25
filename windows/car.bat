a3d4a0f65d155a67798e27c7489d72381fbc7f84
Fox <31753655+xXxsomebodyoncetoldmexXx@users.noreply.github.com>
2020-07-25 13:55:00 +0700

    Fix Errorlevel

--- windows/car.bat
+++ windows/car.bat
@@ -1,8 +1,10 @@
 @echo off
+setlocal EnableDelayedExpansion
 Rem This program compile and run cpp file
 if not [%1]==[] (
-  START /B /WAIT g++ -std=c++17 -Wall -Wextra -Ofast -DLOCAL_PROJECT %1
-  if %ERRORLEVEL% == 0 (
+  rem START /B /WAIT g++ -std=c++17 -Wall -Wextra -Ofast -DLOCAL_PROJECT %1
+  g++ -std=c++17 -Wall -Wextra -Ofast -march=native -DLOCAL_PROJECT %1
+  if !ERRORLEVEL! == 0 (
 		echo [RUN]
 		a.exe
 	)
